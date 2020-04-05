f7aeb1524253fa09515475f8b3389ac848f6b64b
MarioEstebanOchoa <45211069+MarioEstebanOchoa@users.noreply.github.com>
2020-04-05 15:08:49 -0400

    restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,25 @@
-// =======
+// ===========
 // Puerto
-// =======
+// ===========
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ===========
+// Entorno
+// ===========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===========
+// Base datos
+// ===========
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://adminDB:TV2dTLE7mbNkTh98@cluster0-2lxx1.mongodb.net/cafe';
+}
+
+process.env.ULRDB = urlDB;
