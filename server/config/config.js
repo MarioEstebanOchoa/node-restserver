9857ac327ffca38799db1d9d6207feec6ace4eba
MarioEstebanOchoa <45211069+MarioEstebanOchoa@users.noreply.github.com>
2020-04-06 23:03:40 -0400

    login normal

--- server/config/config.js
+++ server/config/config.js
@@ -23,3 +23,19 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.ULRDB = urlDB;
+
+// ===========
+// Vencimiento del token
+// ===========
+//60 segundos
+//60 min
+//24 horas
+//30 dias
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========
+// Seed token
+// ===========
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
